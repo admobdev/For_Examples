cbefe574fa75a52fff69370d9323d4d9dd72713a
ozodb <ozodbekkarimov100@gmail.com>
2022-12-24 11:20:30 +0100

    first comment in terminal

---
+++ app/src/main/java/pl/ozodbek/for_examples/DataBase.java
@@ -0,0 +1,37 @@
+package pl.ozodbek.for_examples;
+
+public class DataBase {
+    public static int[] images = {
+            R.drawable.img_1,
+            R.drawable.img_2,
+            R.drawable.img_3,
+            R.drawable.img_4,
+            R.drawable.img_5,
+            R.drawable.img_6,
+            R.drawable.img_7,
+            R.drawable.img_8,
+            R.drawable.img_9,
+            R.drawable.img_10,
+            R.drawable.img_11,
+            R.drawable.img_12,
+            R.drawable.img_13
+    };
+
+    public static int[] titles = {
+            R.string.java,
+            R.string.javascript,
+            R.string.python,
+            R.string.c,
+            R.string.cplus,
+            R.string.csharp,
+            R.string.typescript,
+            R.string.go,
+            R.string.rust,
+            R.string.sql,
+            R.string.swift,
+            R.string.kotlin,
+            R.string.php,
+            R.string.dart,
+
+    };
+}
